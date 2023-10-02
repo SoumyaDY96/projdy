86c217d722f36766d08d558299abe10b0901ce56
SoumyaDY96 <soumyadeepdey6912@gmail.com>
2023-10-02 15:41:10 +0530

    minor update

--- Accountcheck.sh
+++ Accountcheck.sh
@@ -1,74 +1,59 @@
-#!/bin/bash
-a=true
+#/usr/bin/root/bash
 
+usr=
+while [ -z $usr ]; do
 
-while $a; do
+  echo -e "Please enter Username: "
+  read usr
 
-echo "Please enter Username"
-
-
-read usr
+done
 
 res1=$( grep "^$usr:" /etc/shadow )
 
-
-
 if [ -z $res1 ]; then
-   
-   
+
+
     echo "Account not found"
- 
-    :
 
 else
-
-   
      echo "Account exists"
 
-   
-     res=$( echo $res1 | awk 'BEGIN{FS=":"} { print $2 }' )
-
-     if [[ -z "$res" ]]; then
+fi
 
 
-        echo "Account not locked"
- 
-        :
+res=$( echo $res1 | awk 'BEGIN{FS=":"} { print $2 }' )
+
+if [ -z "$res" ]; then
 
 
-     elif [[ $res == '!' ]]; then
-
- 
-       echo "Account is locked."
-
-        :
-
-    
- elif [[ $res == '!!'  || $res =~ '*' ]]; then
-
- 
-       echo "Account cannot login with a unix password."
-
-        :
+    echo "Account not locked"
 
 
-     else echo "Normal password provided."
+elif [[ $res == '!' ]]; then
 
-            :     
-     fi
+
+    echo "Account is locked"
+
+
+elif [[ $res == '!!'  || $res =~ '*' ]]; then
+
+
+    echo "Account cannot login with a unix password"
+
+
+else echo "Normal password provided"
+
 
 fi
-    
+
 echo "Do you want to continue? type y for yes or n for no"
 
 read ans
 
 if [[ $ans == 'y' ]]; then
 
-    continue
+   ./$0
 
-else echo "Have a good day"; break
+else echo "Have a good day"
 
 fi
-
-done
